99a3e773b2a8b2bcc63251206b011d3a5f8756d3
Manish Kumar <jangramanish2602@gmail.com>
2024-04-28 19:27:58 +0530

    main branch

--- arrshape.py
+++ arrshape.py
@@ -2,6 +2,6 @@ import numpy as np
 arr = np.array([[3,4,5,6],[4,6,5,6]])
 
 #dimensions using ndmin using a vector with values 1,2,3,4 and verify that last dimension has value 4:
-arr2= np.array([1,2,3,4],ndmin=10)
+arr2= np.array([1,2,3,4],ndmin=5)
 print(arr.shape)
 print(arr2.shape)
